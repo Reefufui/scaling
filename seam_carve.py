15f9e32a087c56031eaec2c9a58f14592dfe67e4
Reefufui <reefufui@gmail.com>
2021-10-04 16:27:03 +0300

    fix seam mask needed

---
+++ seam_carve.py
@@ -0,0 +1,139 @@
+import matplotlib
+import numpy as np
+import itertools
+
+from skimage import io
+
+def conversion_from_RGB(img): # яркость изображения
+
+    k = [0.299, 0.587, 0.114]
+    res = 0
+    for i in range(0, 3):
+        res += k[i] * img[:, :, i]
+    return(res.astype("float64"))
+
+def energy_img(img):
+
+    Y = conversion_from_RGB(img)
+    Y_row = Y.shape[0]
+    Y_col = Y.shape[1]
+    I_y, I_x = np.gradient(Y)
+    # Возведём все элементы в квадрат кроме элементов с краю 
+    I_y[1:Y_row - 1, :] *= 2 # производная для игреков считается в строчке как разность нижней и верхней строки
+    I_x[:, 1:Y_col - 1] *= 2 # производная для иксов считается в колонке как разность двух соседних колонок 
+    return(np.sqrt(I_x ** 2 + I_y ** 2).astype("float64"))
+
+def get_y_img(raw_img):
+    h, w, c = raw_img.shape
+    y_img = np.zeros((h, w), dtype='float64')
+
+    xs, ys = np.arange(h), np.arange(w)
+    for x, y in itertools.product(xs, ys):
+        y_img[x, y] = np.dot(raw_img[x, y], np.array([0.299, 0.587, 0.114]))
+
+    return y_img
+
+def get_e_img(y_img):
+    h, w = y_img.shape
+    xs, ys = np.arange(1, w + 1), np.arange(1, h + 1)
+    e_img = np.zeros((h + 2, w + 2), dtype='float64')
+    img = np.pad(y_img, pad_width=1, mode='edge')
+
+    for x, y in itertools.product(xs, ys):
+        dx = img[y + 1, x] - img[y - 1, x]
+        dy = img[y, x + 1] - img[y, x - 1]
+        e_img[y, x] = np.sqrt(dx*dx + dy*dy)
+
+    return e_img[1:-1, 1:-1]
+
+def get_seam_matrix(y_img, mode='horizontal shrink'):
+    h, w = y_img.shape
+    xs, ys = np.arange(1, w + 1), np.arange(1, h + 1)
+    result = np.pad(y_img, pad_width=1, mode='constant', constant_values=(10000000))
+
+    mode = mode.split()[0]
+    if mode == 'horizontal':
+        ys = ys[1:]
+        for y, x in itertools.product(ys, xs):
+            result[y, x] += min(result[y - 1, x - 1 : x + 2])
+    elif mode == 'vertical':
+        xs = xs[1:]
+        for x, y in itertools.product(xs, ys):
+            result[y, x] += min(result[y - 1 : y + 2, x - 1])
+    else:
+        raise ValueError
+
+    result = result[1:-1, 1:-1]
+
+    return result
+
+def get_seam_mask(seam_matrix, mode='horizontal shrink'):
+    h, w = seam_matrix.shape
+    seam_mask = np.zeros((h, w), dtype='int')
+
+    index_of_minimal = lambda arr : np.where(arr == np.amin(arr))[0][0]
+    mode = mode.split()[0]
+    x, y = w - 1, h - 1
+
+    if mode == 'horizontal':
+        x = index_of_minimal(seam_matrix[y])
+        seam_mask[y, x] = 1
+        while y:
+            y -= 1
+            x = min(x+1, w-1) if seam_matrix[y, min(x+1, w-1)] < seam_matrix[y, x] else x
+            x = max(0, x-1) if seam_matrix[y, max(0, x-1)] <= seam_matrix[y, x] else x
+            seam_mask[y, x] = 1
+
+    elif mode == 'vertical':
+        y = index_of_minimal(seam_matrix[..., x])
+        seam_mask[y, x] = 1
+        while x:
+            x -= 1
+            y = min(y+1, h-1) if seam_matrix[min(y+1, h-1), x] < seam_matrix[y, x] else y
+            y = max(0, y-1) if seam_matrix[max(0, y-1), x] <= seam_matrix[y, x] else y
+            seam_mask[y, x] = 1
+
+    else:
+        raise ValueError
+
+    return seam_mask
+
+def shrink(seam_mask, img, mode):
+    mode = mode.split()[0]
+    h, w, c = img.shape
+
+    xs, ys = np.arange(w), np.arange(h)
+    result_shape = (h, w - 1, c) if mode == 'horizontal' else (w, h - 1, c)
+    result = np.zeros(result_shape, dtype='int')
+
+    if mode == 'vertical':
+        xs, ys = np.arange(h), np.arange(w)
+        img = (np.transpose(img[..., 0]), np.transpose(img[..., 1]), np.transpose(img[..., 2]))
+        img = np.dstack(img)
+        seam_mask = np.transpose(seam_mask)
+
+    for y, x in itertools.product(ys, xs):
+        if seam_mask[y][x] == 1:
+            result[y][:x] = img[y][:x]
+            result[y][x:] = img[y][x+1:]
+
+    if mode == 'vertical':
+        result = (np.transpose(result[..., 0]),
+                  np.transpose(result[..., 1]),
+                  np.transpose(result[..., 2]))
+        result = np.dstack(result)
+
+    return result
+
+def seam_carve(img, mode, mask=None):
+    y_img = get_y_img(img)
+    e_img = energy_img(img)
+    #get_e_img(y_img)
+    seam_matrix = get_seam_matrix(e_img, mode)
+    seam_mask = get_seam_mask(seam_matrix, mode)
+
+    result = shrink(seam_mask, img, mode)
+
+
+    return result, mask, seam_mask
+
